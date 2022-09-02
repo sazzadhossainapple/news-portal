267be49f9dfaeae51906e5cf3c98890090c87206
sazzadhossainapple <sazzadapple81@gmail.com>
2022-09-03 00:01:23 +0600

    single category data load

--- js/index.js
+++ js/index.js
@@ -10,18 +10,38 @@ const loadAllCategory = async () => {
   }
 };
 
+// all categorys
 const displayAllCategory = (categorys) => {
-  console.log(categorys);
+  //   console.log(categorys);
 
   const categoryAllList = document.getElementById("category-list");
   categorys.forEach((category) => {
-    const li = document.createElement("li");
-    li.classList.add("nav-item");
-    li.innerHTML = `
-    <a class="nav-link active" aria-current="page" href="#">${category.category_name}</a>
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <li 
+    onclick="loadSingleCategory(${category.category_id})" class="nav-item nav-list-items ">
+    <a class="nav-link active" aria-current="page">${category.category_name}</a>
+    </li>
     `;
-    categoryAllList.appendChild(li);
+    categoryAllList.appendChild(div);
   });
+};
+
+// single Category items
+const loadSingleCategory = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/news/category/${0}${id}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displaySingleCategory(data.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displaySingleCategory = (singleCategorys) => {
+  console.log(singleCategorys);
 };
 
 loadAllCategory();
