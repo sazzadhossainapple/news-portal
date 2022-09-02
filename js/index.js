e0f193a099ee42409dc195733d782eaf1df80ec5
sazzadhossainapple <sazzadapple81@gmail.com>
2022-09-03 01:07:20 +0600

    single category details

--- js/index.js
+++ js/index.js
@@ -42,6 +42,35 @@ const loadSingleCategory = async (id) => {
 
 const displaySingleCategory = (singleCategorys) => {
   console.log(singleCategorys);
+
+  const singleCategoryInfo = document.getElementById("single-category-info");
+  singleCategoryInfo.textContent = "";
+  singleCategorys.forEach((singleCategory) => {
+    const { thumbnail_url, title, details } = singleCategory;
+    const divSingleCategory = document.createElement("div");
+    divSingleCategory.innerHTML = `
+    <div class="card mb-3 p-3">
+          <div class="row g-0">
+            <div class="col-md-3">
+              <img src="${thumbnail_url}" class="img-fluid rounded-start" alt="..." />
+            </div>
+            <div class="col-md-9">
+              <div class="card-body">
+                <h5 class="card-title fw-bold mb-3">${title}</h5>
+                <p class="card-text text-muted lh-lg">${
+                  details.length > 300 ? details.slice(0, 300) + "..." : details
+                }</p>
+                <p class="card-text">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+     
+    `;
+    singleCategoryInfo.appendChild(divSingleCategory);
+  });
 };
 
 loadAllCategory();
