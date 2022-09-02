361b1b7af162749bdf59880872a25add1c911e23
sazzadhossainapple <sazzadapple81@gmail.com>
2022-09-02 22:42:40 +0600

    category loaded

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-console.log("conneted");
+const loadAllCategory = async () => {
+  const url = "https://openapi.programming-hero.com/api/news/categories";
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    displayAllCategory(data.data.news_category);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const displayAllCategory = (categorys) => {
+  console.log(categorys);
+
+  const categoryAllList = document.getElementById("category-list");
+  categorys.forEach((category) => {
+    const li = document.createElement("li");
+    li.classList.add("nav-item");
+    li.innerHTML = `
+    <a class="nav-link active" aria-current="page" href="#">${category.category_name}</a>
+    `;
+    categoryAllList.appendChild(li);
+  });
+};
+
+loadAllCategory();
